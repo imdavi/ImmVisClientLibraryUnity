83ad5a32d2d3b5384c360c69122cdc5fc6f62f08
Felipe Pedroso <f.pedroso677@gmail.com>
2020-08-22 16:43:01 +0100

    Creating Scatterplot sample.

---
+++ Assets/ImmVisClientGrpcUnity/Examples/Scatterplot/Scripts/DataManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ImmVis.Grpc;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public ImmVisGrpcClientManager immVisGrpcClientManager;
+
+    public ScatterplotBehaviour scatterplotBehaviour;
+
+    void Start()
+    {
+        if (immVisGrpcClientManager != null)
+        {
+            immVisGrpcClientManager.ClientInitialized += ClientIsReady;
+        }
+    }
+
+    private async void ClientIsReady()
+    {
+        Debug.Log("The GrpcClient is ready to play!");
+
+        var grpcClient = immVisGrpcClientManager.GrpcClient;
+
+        var availableDatasets = await grpcClient.ListAvailableDatasetsAsync(new Empty());
+
+        var datasetsPaths = availableDatasets.DatasetsPaths;
+
+        if (datasetsPaths.Count > 0)
+        {
+            var datasetPath = datasetsPaths[0];
+
+            var datasetMetadata = await grpcClient.LoadDatasetAsync(new LoadDatasetRequestMessage()
+            {
+                DatasetPath = datasetPath
+            });
+
+            var pointsCount = datasetMetadata.RowsCount;
+
+            var datasetToPlot = await grpcClient.GetDatasetToPlotAsync(new GetDatasetToPlotRequestMessage());
+
+            scatterplotBehaviour?.PlotData(datasetToPlot);
+        }
+    }
+}
---
+++ Assets/ImmVisClientGrpcUnity/Examples/Scatterplot/Scripts/RotationBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationBehaviour : MonoBehaviour
+{
+
+    public float RotationMultiplier = 1f;
+
+    public void RotateX(float speed)
+    {
+        transform.Rotate(speed * RotationMultiplier, 0f, 0f, Space.Self);
+    }
+
+    public void RotateY(float speed)
+    {
+        transform.Rotate(0f, speed * RotationMultiplier, 0f, Space.Self);
+    }
+
+    public void RotateZ(float speed)
+    {
+        transform.Rotate(0f, 0f, speed * RotationMultiplier, Space.Self);
+    }
+
+    public void ResetRotation()
+    {
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+    }
+}
